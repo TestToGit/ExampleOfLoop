22f04752d4ebaedc4ee188d5602dbb9583fd0ac2
TestToGit <sarveshagrahari22@gmail.com>
2025-01-02 19:57:54 +0800

    this is new changeses

---
+++ Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfLoop
+{
+    class Class3
+    {
+        public void Substract(int Num1 , int Num2)
+        {
+            int result = Num1 - Num2;   
+            Console.WriteLine("Substract of Two Number " + result);
+        }
+       
+    }
+   
+}
