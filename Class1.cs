25e511937d8eb54a63904922e80a49a9fc6e70db
TestToGit <sarveshagrahari22@gmail.com>
2025-01-20 19:27:16 +0800

    third commit

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfLoop
+{
+    internal class Class1
+    {
+        public void show()
+        {
+            Console.WriteLine("this is first class ");
+        }
+    }
+}
